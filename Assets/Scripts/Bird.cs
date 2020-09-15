73fe4590a888c3ee49f88403b784ffb249d57e21
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-15 17:40:18 +0800

    Add bird script

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,18 +1,70 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum BirdState{ Idle, Thrown}
+    public GameObject parent;
+    public Rigidbody2D birdRigidbody;
+    public CircleCollider2D birdCollider;
+
+    private BirdState birdState;
+    private float minVelocity = 0.05f;
+    private bool flagDestroy = false;
+
+    private void Start()
     {
-        
+        birdRigidbody.bodyType = RigidbodyType2D.Kinematic;
+        birdCollider.enabled = false;
+        birdState = BirdState.Idle;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (birdState == BirdState.Idle &&
+                        birdRigidbody.velocity.sqrMagnitude >= minVelocity)
+        {
+            birdState = BirdState.Thrown;
+        }
+
+        if (birdState == BirdState.Thrown &&
+            birdRigidbody.velocity.sqrMagnitude < minVelocity &&
+            !flagDestroy)
+        {
+            // Destroy object after 2 seconds
+            // if its speed is less than minimum velocity
+            flagDestroy = true;
+            StartCoroutine(DestroyAfter(2));
+        }
+    }
+    
+    private IEnumerator DestroyAfter(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
+    }
+    
+    /// <summary>
+    /// Initiate position and change bird's parent
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="parent"></param>
+    public void MoveTo(Vector2 target, GameObject parent)
+    {
+        gameObject.transform.SetParent(parent.transform);
+        gameObject.transform.position = target;
+    }
+    
+    /// <summary>
+    /// Throw bird to direction, distance and velocity
+    /// </summary>
+    /// <param name="velocity"></param>
+    /// <param name="distance"></param>
+    /// <param name="speed"></param>
+    public void Shoot(Vector2 velocity, float distance, float speed)
+    {
+        birdCollider.enabled = true;
+        birdRigidbody.bodyType = RigidbodyType2D.Dynamic;
+        birdRigidbody.velocity = velocity * speed * distance;
     }
 }
