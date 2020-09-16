88eaf9ccc84c041d8bbbb2d663dc4736ec72cc57
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-16 15:31:08 +0800

    Make destroyer

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        string tag = other.tag;
+
+        if (tag == "Bird" || tag == "Enemy" || tag == "Obstacle")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
