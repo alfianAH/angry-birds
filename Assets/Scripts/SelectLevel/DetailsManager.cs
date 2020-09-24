4dc05d952f304447cac1a7350e3470ca62f75695
Alfian Hamdani <alfian.hamdani04@gmail.com>
2020-09-24 17:12:19 +0800

    Make select level scriptable objects

---
+++ Assets/Scripts/SelectLevel/DetailsManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DetailsManager : MonoBehaviour
+{
+    public LevelScriptableObject levelScriptableObject;
+    [SerializeField] private Text levelName;
+
+    private bool seeDetails;
+
+    public bool SeeDetails
+    {
+        get => seeDetails;
+        set => seeDetails = value;
+    }
+    
+    public void Update()
+    {
+        if(levelScriptableObject && seeDetails)
+        {
+            seeDetails = false;
+            levelName.text = levelScriptableObject.levelName;
+        }
+    }
+
+    /// <summary>
+    /// PlayBtn in Details
+    /// </summary>
+    /// <param name="sceneLoader"></param>
+    public void Play(SceneLoader sceneLoader)
+    {
+        if (sceneLoader)
+        {
+            sceneLoader.LoadScene(levelScriptableObject.sceneName);
+        }
+    }
+}
---
+++ Assets/Scripts/SelectLevel/LevelUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUI : MonoBehaviour
+{
+    public LevelScriptableObject levelScriptableObject;
+
+    [SerializeField] private Text levelText;
+    [SerializeField] private DetailsManager detailsManager;
+    
+    private void Awake()
+    {
+        levelText.text = levelScriptableObject.levelName;
+    }
+
+    public void SetDetailsUi()
+    {
+        detailsManager.gameObject.SetActive(true);
+        detailsManager.levelScriptableObject = levelScriptableObject;
+        detailsManager.SeeDetails = true;
+    }
+}
